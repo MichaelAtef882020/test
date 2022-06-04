2df44643cac8d7fd6823659b9a172c95aafb375b
GeorgeElkess <100765793+GeorgeElkess@users.noreply.github.com>
2022-06-04 05:04:50 +0200

    .

--- Classes/CashPayment.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "PaymentInterface.php";
-class Cash extends IPay {
-    public function Pay($OrderId) {
-        $this->FileManger->Add("Paying CASH for order ".$OrderId."\r\n");
-    }
-}
