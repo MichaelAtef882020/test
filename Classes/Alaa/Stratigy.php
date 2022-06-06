fbd493d6010a591cfccb80a489bfcc59b07db09e
AlaaTaie <100770980+AlaaTaie@users.noreply.github.com>
2022-06-07 01:14:44 +0200

    ..

---
+++ Classes/Alaa/Stratigy.php
@@ -0,0 +1,31 @@
+<?php
+include_once "Order Payments";
+include_once "File Manager";
+interface Ipay {
+    public function Pay($OrderId);
+}
+class Fawry implements Ipay
+{
+    private FileManger $ObjFileManager;
+    public function Pay($OrderId)
+    {
+        $this->ObjFileManager->Add("Pay Cash for $OrderId");
+    }
+}
+class Cash implements Ipay
+{
+
+    private FileManger $ObjFileManager;
+    public function Pay($OrderId)
+    {
+        $this->ObjFileManager->Add("Pay Cash for $OrderId");
+    }
+}
+class Visa implements Ipay
+{
+    private FileManger $ObjFileManager;
+    public function Pay($OrderId)
+    {
+        $this->ObjFileManager->Add("Pay Visa for $OrderId");
+    }
+}
